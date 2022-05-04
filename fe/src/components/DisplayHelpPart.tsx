5eeb7f183f6cbf587daf11a98250748012c10f61
Dominik Bydžovský <dominikbydzovsky94@gmail.com>
2022-05-04 18:43:47 +0200

    crop images

--- fe/src/components/DisplayHelpPart.tsx
+++ fe/src/components/DisplayHelpPart.tsx
@@ -9,23 +9,26 @@ export interface Props {
 export function DisplayHelpPart(props: Props) {
     const classes = useStyles()
     return <div className={classes.root}>
-        <ScreenShowcase img={Images.mainscreen1}/>
-        <ScreenShowcase img={Images.mainScreen2}/>
-        <ScreenShowcase img={Images.alarmScreen}/>
-        <ScreenShowcase img={Images.historyCo2Screen}/>
-        <ScreenShowcase img={Images.tempHistory}/>
-        <ScreenShowcase img={Images.disableScreen}/>
-        <ScreenShowcase img={Images.party}/>
-        <ScreenShowcase img={Images.manualScreen}/>
-        <ScreenShowcase img={Images.inhouseScreen}/>
-        <ScreenShowcase img={Images.outscreen}/>
-        <ScreenShowcase img={Images.debug}/>
+        <ScreenShowcase img={Images.mainscreen1} txt={"Hlavní obrazovka zobrazuje aktuální čas, stav připojení k wifi, teplotu v kolektoru a CO2 hodnotu."}/>
+        <ScreenShowcase img={Images.mainScreen2} txt={"Aktuální čas se střídá se zobrazováním aktuální předpovědi pocitové teploty na následující den."}/>
+        <ScreenShowcase img={Images.alarmScreen} txt={"V případě neočekávaných problémů problikává hlavní obrazovka podobným textem."}/>
+        <ScreenShowcase img={Images.historyCo2Screen} txt={"Historie naměřených CO2 hodnot za poslední 2 hodiny. Čísla v grafu uvádějí krajní hodnoty."}/>
+        <ScreenShowcase img={Images.tempHistory} txt={"Historie naměřených hodnot teploty v kolektoru za poslední 2 hodiny. Čísla v grafu uvádějí krajní hodnoty."}/>
+        <ScreenShowcase img={Images.disableScreen} txt={"Obrazovka s akcí, kterou je možné dočasně vypnout zařízení."}/>
+        <ScreenShowcase img={Images.party} txt={"Obrazovka s akcí, kterou je možné zapnout párty mód."}/>
+        <ScreenShowcase img={Images.manualScreen} txt={"Manuální kontrola zařízení. Ukazuje aktuální teplotu v kolektoru, aktuální CO2 a čas, který zbývá do konce programu."}/>
+        <ScreenShowcase img={Images.inhouseScreen} txt={"Informace o vnitřních čidlech. Ukazuje aktuální rosný bod, teplotu, relativní vlhkost a CO2."}/>
+        <ScreenShowcase img={Images.outscreen} txt={"Informace o venkovních čidlech. Ukazuje aktuální rosný bod, teplotu a relativní vlhkost v kolektoru a předpovídanou pocitovou teplotu na následující den."}/>
+        <ScreenShowcase img={Images.debug} txt={"Ladící obrazovka. Zobrazuje aktuální verzi programu, množství zbývající paměti, celkový čas běhu, číslo programu a rok výroby."}/>
     </div>
 }
-
-function ScreenShowcase(props: {img: {src: string}}) {
+interface ShowCaseProps {
+    txt: string
+    img: {src: string}
+}
+function ScreenShowcase(props: ShowCaseProps) {
     return <div>
-        {props.img.src}
+        <img src={props.img.src}/>
     </div>
 }
 
